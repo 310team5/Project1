6c6c54daa110d26eb975b02431838614152fd5ec
Cameron Nathan <79772516+cnat307@users.noreply.github.com>
2022-08-15 15:58:09 +1200

    refactor: created constants for the timer

--- src/application/scene/Timer.java
+++ src/application/scene/Timer.java
@@ -6,17 +6,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.text.Text;
 
-public class Timer {
-	  @FXML private Button button_dashboard;
+public class Timer{
 
 	  @FXML private ToggleButton pomodoroButton, shortBreakButton, longBreakButton;
-
 	  @FXML private Text countdownText;
+	  @FXML private Button startButton;
+
+	  private static final String POMODORO_TIME = "25:00";
+	  private static final String SHORT_BREAK_TIME = "05:00";
+	  private static final String LONG_BREAK_TIME = "15:00";
+	  private static final String DISABLE_BUTTON_STYLE = "-fx-opacity: 1.0;";
 
 	  @FXML
 	  public void initialize(){
 		  pomodoroButton.fire();
-		  countdownText.setText("25:00");
+		  countdownText.setText(POMODORO_TIME);
 	  }
 
 	  /** Click handler for the dashboard button. */
@@ -29,30 +33,30 @@ public class Timer {
 	  public void selectTimerButton(ActionEvent event){
 		   if (pomodoroButton.isSelected()){
 			   pomodoroButton.setDisable(true);
-			   pomodoroButton.setStyle("-fx-opacity: 1.0;");
+			   pomodoroButton.setStyle(DISABLE_BUTTON_STYLE);
 
 			   shortBreakButton.setDisable(false);
 			   longBreakButton.setDisable(false);
 
-			   countdownText.setText("25:00");
+			   countdownText.setText(POMODORO_TIME);
 
 		   } else if (shortBreakButton.isSelected()){
 			   shortBreakButton.setDisable(true);
-			   shortBreakButton.setStyle("-fx-opacity: 1.0;");
+			   shortBreakButton.setStyle(DISABLE_BUTTON_STYLE);
 
 			   pomodoroButton.setDisable(false);
 			   longBreakButton.setDisable(false);
 
-			   countdownText.setText("05:00");
+			   countdownText.setText(SHORT_BREAK_TIME);
 
 		   } else if (longBreakButton.isSelected()){
 			   longBreakButton.setDisable(true);
-			   longBreakButton.setStyle("-fx-opacity: 1.0;");
+			   longBreakButton.setStyle(DISABLE_BUTTON_STYLE);
 
 			   pomodoroButton.setDisable(false);
 			   shortBreakButton.setDisable(false);
 
-			   countdownText.setText("15:00");
+			   countdownText.setText(LONG_BREAK_TIME);
 		   }
 	  }
 }
