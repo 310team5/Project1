258117751139e9469f8ab38be464fa7bc198fb1e
hjia095 <hjia095@aucklanduni.ac.nz>
2022-08-22 10:15:17 +1200

    Add Local file system to store todo lists

--- src/application/scene/Todo.java
+++ src/application/scene/Todo.java
@@ -3,7 +3,9 @@ package application.scene;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.io.*;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Todo {
     @FXML
@@ -14,6 +16,8 @@ public class Todo {
     private Button button_moveDOING;
     @FXML
     private Button button_moveDONE;
+    @FXML
+    private Button button_clearDONE;
     @FXML
     private TextField textField_eventName;
     @FXML
@@ -27,7 +31,6 @@ public class Todo {
     @FXML
     private ChoiceBox<String> choiceBox_status;
 
-    Alert alert_noContentWarning = new Alert(Alert.AlertType.WARNING);
 
     public void initialize() {
         datePicker.setValue(LocalDate.now());
@@ -36,31 +39,37 @@ public class Todo {
         choiceBox_status.getItems().addAll("TODO", "DOING", "DONE");
         choiceBox_status.setValue("TODO");
 
+        getHistory(listView_TODO, "TODO");
+        getHistory(listView_DOING, "DOING");
+        getHistory(listView_DONE, "DONE");
+
 
     }
 
     @FXML
     private void addEvent() {
         String status = choiceBox_status.getValue();
+        String eventMessage = "At " + datePicker.getValue() + " " + textField_eventName.getText();
 
         if (textField_eventName.getText().isEmpty()) {
             noContentAlert();
         } else {
             switch (status) {
                 case "TODO":
-                    listView_TODO.getItems().add("At " + datePicker.getValue() + " " + textField_eventName.getText());
+                    addToList(listView_TODO, "TODO", eventMessage);
                     break;
                 case "DOING":
-                    listView_DOING.getItems().add("123");
+                    addToList(listView_DOING, "TODO", eventMessage);
                     break;
                 case "DONE":
-                    listView_DONE.getItems().add("123");
+                    addToList(listView_DONE, "TODO", eventMessage);
                     break;
                 default:
                     break;
             }
         }
     }
+
 
     @FXML
     private void moveToDOING() {
@@ -69,6 +78,8 @@ public class Todo {
             listView_DOING.getItems().add(currentItem);
             listView_TODO.getItems().remove(currentItem);
 
+            rewriteHistory(listView_TODO, "TODO");
+            rewriteHistory(listView_DOING, "DOING");
         }
     }
 
@@ -79,10 +90,81 @@ public class Todo {
             listView_DONE.getItems().add(currentItem);
             listView_DOING.getItems().remove(currentItem);
 
+            rewriteHistory(listView_DOING, "DOING");
+            rewriteHistory(listView_DONE, "DONE");
         }
     }
 
+    @FXML
+    private void clearDONE() {
+        Alert alert_clearDONEConfirmation = new Alert(Alert.AlertType.CONFIRMATION, "Do you really want to remove all Events in this area", ButtonType.YES, ButtonType.NO);
+        ButtonType result = alert_clearDONEConfirmation.showAndWait().orElse(ButtonType.NO);
+
+        if (ButtonType.YES.equals(result)) {
+            listView_DONE.getItems().clear();
+        }
+
+        try {
+            File file = new File("./src/application/scene/ToDoLists/DONE.txt");
+            FileWriter clearWriter = new FileWriter(file); //the true will append the new data
+            clearWriter.write("");
+            clearWriter.close();
+        } catch (IOException e) {
+        }
+
+    }
+
+    private void addToList(ListView<String> listView, String status, String eventMessage) {
+        try {
+
+            File file = new File("./src/application/scene/ToDoLists/" + status + ".txt");
+            FileWriter attributeWriter = new FileWriter(file, true);
+
+            listView.getItems().add(eventMessage);
+            attributeWriter.write(eventMessage + "\n");//appends the string to the file
+            attributeWriter.close();
+        } catch (IOException e) {
+
+        }
+
+    }
+
+
+    private void getHistory(ListView<String> listName, String status) {
+        try {
+            String file = "./src/application/scene/ToDoLists/" + status + ".txt";
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while (((line = br.readLine()) != null)) {
+                listName.getItems().add(line);
+            }
+            br.close();
+
+        } catch (IOException ex) {
+        }
+    }
+
+    private void rewriteHistory(ListView<String> listName, String status) {
+        try {
+            File file = new File("./src/application/scene/ToDoLists/" + status + ".txt");
+            FileWriter clearWriter = new FileWriter(file); //the true will append the new data
+            clearWriter.write("");
+            clearWriter.close();
+
+            FileWriter attributeWriter = new FileWriter(file, true);
+            List<String> history = listName.getItems();
+            for (String thisAttribute : history) {
+                attributeWriter.write(thisAttribute + "\n");//appends the string to the file
+
+            }
+            attributeWriter.close();
+        } catch (IOException e) {
+        }
+    }
+
+
     public void noContentAlert() {
+        Alert alert_noContentWarning = new Alert(Alert.AlertType.WARNING);
         // set content text
         alert_noContentWarning.setContentText("Please Fill Event Name");
 
