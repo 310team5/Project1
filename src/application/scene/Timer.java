e2ef08d906db1f7c676d4ec446494cfea7faad44
Cameron Nathan <79772516+cnat307@users.noreply.github.com>
2022-08-23 16:33:02 +1200

    implemented navigation to other tools

--- src/application/scene/Timer.java
+++ src/application/scene/Timer.java
@@ -12,143 +12,161 @@ import javafx.util.Duration;
 
 public class Timer{
 
-	  @FXML private ToggleButton pomodoroButton, shortBreakButton, longBreakButton;
-	  @FXML private ToggleGroup timerButtons;
-	  @FXML private Text countdownText;
-	  @FXML private Button startButton;
+	@FXML private ToggleButton pomodoroButton, shortBreakButton, longBreakButton;
+	@FXML private ToggleGroup timerButtons;
+	@FXML private Text countdownText;
+	@FXML private Button startButton;
 
-	  private static final String POMODORO_TIME = "25:00";
-	  private static final String SHORT_BREAK_TIME = "05:00";
-	  private static final String LONG_BREAK_TIME = "15:00";
-	  private static final String DISABLE_BUTTON_STYLE = "-fx-opacity: 1.0;";
-	  private Time time = new Time();
-	  private int numOfPomodoro = 0;
-	  private static final int MAX_NUM_OF_POMODORO = 4;
+	private static final String POMODORO_TIME = "25:00";
+	private static final String SHORT_BREAK_TIME = "05:00";
+	private static final String LONG_BREAK_TIME = "15:00";
+	private static final String DISABLE_BUTTON_STYLE = "-fx-opacity: 1.0;";
+	private Time time = new Time();
+	private int numOfPomodoro = 0;
+	private static final int MAX_NUM_OF_POMODORO = 4;
 
-	  private Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
+	private Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
 
-		  // When one of the break timers finish, switch timer to prepare for a pomodoro
-		  if (time.getMinutes()==0 && time.getSeconds()==0 && (shortBreakButton.isSelected() || longBreakButton.isSelected())){
-			  timerButtons.selectToggle(pomodoroButton);
-			  preparePomodoro();
-			  stopTimer();
+		// When one of the break timers finish, switch timer to prepare for a pomodoro
+		if (time.getMinutes()==0 && time.getSeconds()==0 && (shortBreakButton.isSelected() || longBreakButton.isSelected())){
+			timerButtons.selectToggle(pomodoroButton);
+			preparePomodoro();
+			stopTimer();
 
-		  /* When the pomodoro timer finishes, switch timer to prepare for a break.
-		   * Timer switches to a short break after one pomodoro and a long break after four pomodoro */
-		  } else if (time.getMinutes()==0 && time.getSeconds()==0 && pomodoroButton.isSelected()){
-			  numOfPomodoro++;
+			/* When the pomodoro timer finishes, switch timer to prepare for a break.
+			 * Timer switches to a short break after one pomodoro and a long break after four pomodoro */
+		} else if (time.getMinutes()==0 && time.getSeconds()==0 && pomodoroButton.isSelected()){
+			numOfPomodoro++;
 
-			  if (numOfPomodoro < MAX_NUM_OF_POMODORO){
-				  timerButtons.selectToggle(shortBreakButton);
-				  prepareShortBreak();
-				  stopTimer();
+			if (numOfPomodoro < MAX_NUM_OF_POMODORO){
+				timerButtons.selectToggle(shortBreakButton);
+				prepareShortBreak();
+				stopTimer();
 
-			  } else if (numOfPomodoro == MAX_NUM_OF_POMODORO) {
-				  numOfPomodoro = 0;
+			} else if (numOfPomodoro == MAX_NUM_OF_POMODORO) {
+				numOfPomodoro = 0;
 
-				  timerButtons.selectToggle(longBreakButton);
-				  prepareLongBreak();
-				  stopTimer();
-			  }
+				timerButtons.selectToggle(longBreakButton);
+				prepareLongBreak();
+				stopTimer();
+			}
 
-		  } else {
+		} else {
 
-			  time.secondCountDown();
-			  countdownText.setText(time.getCurrentTime());
-		  }
-	  }));
+			time.secondCountDown();
+			countdownText.setText(time.getCurrentTime());
+		}
+	}));
 
-	  @FXML
-	  public void initialize(){
-		  // The default timer setting is pomodoro
-		  pomodoroButton.fire();
-		  countdownText.setText(POMODORO_TIME);
-		  time.setCurrentTime(POMODORO_TIME);
+	@FXML
+	public void initialize(){
+		// The default timer setting is pomodoro
+		pomodoroButton.fire();
+		countdownText.setText(POMODORO_TIME);
+		time.setCurrentTime(POMODORO_TIME);
 
-	  }
+	}
 
-	  /** Click handler for the dashboard button. */
-	  @FXML
-	  private void dashboard() {
-	    SceneManager.switchToDashboardScene();
-	  }
+	/** Click handler for the dashboard button. */
+	@FXML
+	private void dashboard() {
+		SceneManager.switchToDashboardScene();
+	}
 
-	  /** Click handler for the timer toggle buttons */
-	  @FXML
-	  public void selectTimerButton(ActionEvent event){
-		   if (pomodoroButton.isSelected()){
-			   preparePomodoro();
-			   stopTimer();
+	/** Click handler for the notes button. */
+	@FXML
+	private void notes() {
+		SceneManager.switchToNotesScene();
+	}
 
-		   } else if (shortBreakButton.isSelected()){
-			   prepareShortBreak();
-			   stopTimer();
+	/** Click handler for the todo button. */
+	@FXML
+	private void todo() {
+		SceneManager.switchToTodoScene();
+	}
 
-		   } else if (longBreakButton.isSelected()){
-			   prepareLongBreak();
-			   stopTimer();
+	/** Click handler for the drawboard button. */
+	@FXML
+	private void drawboard() {
+		SceneManager.switchToDrawboardScene();
+	}
 
-		   }
-	  }
+	/** Click handler for the timer toggle buttons */
+	@FXML
+	public void selectTimerButton(ActionEvent event){
+		if (pomodoroButton.isSelected()){
+			preparePomodoro();
+			stopTimer();
+
+		} else if (shortBreakButton.isSelected()){
+			prepareShortBreak();
+			stopTimer();
+
+		} else if (longBreakButton.isSelected()){
+			prepareLongBreak();
+			stopTimer();
+
+		}
+	}
 
 	/**
 	 *  The three prepare methods disable the chosen timer toggle button so that it cannot be
 	 *  unselected. The button style is changed to address the change in opacity when a button
 	 *  is disabled.
 	 */
-	  private void preparePomodoro(){
-		  pomodoroButton.setDisable(true);
-		  pomodoroButton.setStyle(DISABLE_BUTTON_STYLE);
+	private void preparePomodoro(){
+		pomodoroButton.setDisable(true);
+		pomodoroButton.setStyle(DISABLE_BUTTON_STYLE);
 
-		  shortBreakButton.setDisable(false);
-		  longBreakButton.setDisable(false);
+		shortBreakButton.setDisable(false);
+		longBreakButton.setDisable(false);
 
-		  countdownText.setText(POMODORO_TIME);
-		  time.setCurrentTime(POMODORO_TIME);
-	  }
+		countdownText.setText(POMODORO_TIME);
+		time.setCurrentTime(POMODORO_TIME);
+	}
 
-	  private void prepareShortBreak(){
-		  shortBreakButton.setDisable(true);
-		  shortBreakButton.setStyle(DISABLE_BUTTON_STYLE);
+	private void prepareShortBreak(){
+		shortBreakButton.setDisable(true);
+		shortBreakButton.setStyle(DISABLE_BUTTON_STYLE);
 
-		  pomodoroButton.setDisable(false);
-		  longBreakButton.setDisable(false);
+		pomodoroButton.setDisable(false);
+		longBreakButton.setDisable(false);
 
-		  countdownText.setText(SHORT_BREAK_TIME);
-		  time.setCurrentTime(SHORT_BREAK_TIME);
-	  }
+		countdownText.setText(SHORT_BREAK_TIME);
+		time.setCurrentTime(SHORT_BREAK_TIME);
+	}
 
-	  private void prepareLongBreak(){
-		  longBreakButton.setDisable(true);
-		  longBreakButton.setStyle(DISABLE_BUTTON_STYLE);
+	private void prepareLongBreak(){
+		longBreakButton.setDisable(true);
+		longBreakButton.setStyle(DISABLE_BUTTON_STYLE);
 
-		  pomodoroButton.setDisable(false);
-		  shortBreakButton.setDisable(false);
+		pomodoroButton.setDisable(false);
+		shortBreakButton.setDisable(false);
 
-		  countdownText.setText(LONG_BREAK_TIME);
-		  time.setCurrentTime(LONG_BREAK_TIME);
-	  }
+		countdownText.setText(LONG_BREAK_TIME);
+		time.setCurrentTime(LONG_BREAK_TIME);
+	}
 
 	/** Click handler for the Start/Stop button. */
-	  @FXML
-	  public void controlTimer(ActionEvent event){
-		  String timerStatus = startButton.getText();
-		  if (timerStatus.equals("START")){
-			  startTimer();
-		  } else if (timerStatus.equals("STOP")) {
-			  stopTimer();
-		  }
-	  }
+	@FXML
+	public void controlTimer(ActionEvent event){
+		String timerStatus = startButton.getText();
+		if (timerStatus.equals("START")){
+			startTimer();
+		} else if (timerStatus.equals("STOP")) {
+			stopTimer();
+		}
+	}
 
-	  public void startTimer(){
-		  timeline.setCycleCount(Timeline.INDEFINITE);
-		  timeline.play();
-		  startButton.setText("STOP");
-	  }
+	public void startTimer(){
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.play();
+		startButton.setText("STOP");
+	}
 
-	  public void stopTimer(){
-		  timeline.pause();
-		  startButton.setText("START");
-	  }
+	public void stopTimer(){
+		timeline.pause();
+		startButton.setText("START");
+	}
 }
 
